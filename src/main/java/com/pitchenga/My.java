df72d60c93d12f46a5cb78ccdafba85b9b52c8aa
din tort <dintort@gmail.com>
2021-03-06 07:51:10 +0100

    Adding alternating instruments (in a very dirty way)

--- src/main/java/com/pitchenga/My.java
+++ src/main/java/com/pitchenga/My.java
@@ -38,11 +38,11 @@ public class My extends Setup {
         //fixme: Delay hinter proportionally to the tempo
 
         defaultPacer = Pacer.Answer;
-        defaultPacer = Pacer.Tempo100;
         defaultPacer = Pacer.Tempo120;
+        defaultPacer = Pacer.Tempo100;
 
-        defaultRiddler = Riddler.Step43Octaves2And3And4And5And6SiDo;
         defaultRiddler = Riddler.Step43Octave6;
+        defaultRiddler = Riddler.Step43Octaves2And3And4And5And6SiDo;
 //        defaultRiddler = Riddler.Step50Guitar;
 
         defaultRinger = Ringer.Tone;
