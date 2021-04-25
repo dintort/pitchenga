f6c5e47320a221674d3a28bbc41e4c630036fa06
din tort <dintort@gmail.com>
2021-04-25 02:22:16 +0200

    Minor - squircle icon (but something is wrong with size)

--- ptchng.sh
+++ ptchng.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+#jar2app Pitchenga.jar --name=Pitchenga --display-name=Pitchenga -b com.pitchenga.Pitchenga -v 0.0.1 -s 0.0.1 -m com.pitchenga.Ptchng -o --icon=../resources/main/pitchenga.png
+
+
 echo home=$HOME
-JDK=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+#JDK=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+JRE=/Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home
 #JDK=/c/Program\ Files/RedHat/java-1.8.0-openjdk-1.8.0.212-3
 PROJECT=$HOME/dev/pitchenga
 echo project=$PROJECT
-"$JDK/bin/java" -classpath "$JDK/jre/lib/charsets.jar":"$JDK/jre/lib/ext/cldrdata.jar":"$JDK/jre/lib/ext/dnsns.jar":"$JDK/jre/lib/ext/jaccess.jar":"$JDK/jre/lib/ext/localedata.jar":"$JDK/jre/lib/ext/nashorn.jar":"$JDK/jre/lib/ext/sunec.jar":"$JDK/jre/lib/ext/sunjce_provider.jar":"$JDK/jre/lib/ext/sunpkcs11.jar":"$JDK/jre/lib/ext/zipfs.jar":"$JDK/jre/lib/jce.jar":"$JDK/jre/lib/jsse.jar":"$JDK/jre/lib/management-agent.jar":"$JDK/jre/lib/resources.jar":"$JDK/jre/lib/rt.jar":"$JDK/lib/dt.jar":"$JDK/lib/jconsole.jar":"$JDK/lib/sa-jdi.jar":"$JDK/lib/tools.jar":"$PROJECT/out/production/classes":"$PROJECT/out/production/resources":"$PROJECT/lib/TarsosDSP-2.4.jar" com.pitchenga.Ptchng -NSRequiresAquaSystemAppearance False
+#"$JRE/bin/java" -classpath "$JRE/lib/charsets.jar":"$JRE/lib/ext/cldrdata.jar":"$JRE/lib/ext/dnsns.jar":"$JRE/lib/ext/jaccess.jar":"$JRE/lib/ext/localedata.jar":"$JRE/lib/ext/nashorn.jar":"$JRE/lib/ext/sunec.jar":"$JRE/lib/ext/sunjce_provider.jar":"$JRE/lib/ext/sunpkcs11.jar":"$JRE/lib/ext/zipfs.jar":"$JRE/lib/jce.jar":"$JRE/lib/jsse.jar":"$JRE/lib/management-agent.jar":"$JRE/lib/resources.jar":"$JRE/lib/rt.jar":"$JDK/lib/dt.jar":"$JDK/lib/jconsole.jar":"$JDK/lib/sa-jdi.jar":"$JDK/lib/tools.jar":"$PROJECT/out/production/classes":"$PROJECT/out/production/resources":"$PROJECT/lib/TarsosDSP-2.4.jar" com.pitchenga.Ptchng -NSRequiresAquaSystemAppearance False
+"$JRE/bin/java" -classpath "$PROJECT/out/production/classes":"$PROJECT/out/production/resources":"$PROJECT/lib/TarsosDSP-2.4.jar" com.pitchenga.Ptchng -NSRequiresAquaSystemAppearance False
