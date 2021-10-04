8d7982a5f182b1ddd08c716590e3b1eac7870b3f
din tort <dintort@gmail.com>
2021-10-04 20:58:53 +0200

    Adding more third-party libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,28 +9,34 @@ java {
 }
 
 repositories {
-    jcenter()
-    maven {
-        url = uri("https://mvnrepository.com/artifact/")
-    }
-    mavenCentral()
+//    jcenter()
+//    maven {
+//        url = uri("https://mvnrepository.com/artifact/")
+//    }
+//    mavenCentral()
 //    google()
 }
 
 dependencies {
-    implementation(files("lib/TarsosDSP-2.4.jar"))
     //implementation 'colt:colt:1.2.0'
-    implementation("edu.emory.mathcs:JTransforms:2.4")
-    implementation("org.apache.commons:commons-math3:3.2")
-    implementation("org.apache.commons:commons-lang3:3.1")
-    implementation("org.simplericity.macify:macify:1.6")
-    implementation("javazoom:jlayer:1.0.1")
-    implementation("com.googlecode.soundlibs:mp3spi:1.9.5.4")
-    implementation("com.googlecode.soundlibs:tritonus-share:0.3.7.4")
+    implementation(files("lib/TarsosDSP-2.4.jar"))
+    implementation(files("lib/TarsosDSP-2.4.jar"))
+    implementation(files("lib/JTransforms-2.4.jar"))
+    implementation(files("lib/commons-math3-3.2.jar"))
+    implementation(files("lib/commons-lang3-3.1.jar"))
+    implementation(files("lib/jlayer-1.0.1.4.jar"))
+    implementation(files("lib/macify-1.6.jar"))
+    implementation(files("lib/mp3spi-1.9.5.4.jar"))
+    implementation(files("lib/tritonus-share-0.3.7.4.jar"))
+//    implementation("edu.emory.mathcs:JTransforms:2.4")
+//    implementation("org.apache.commons:commons-math3:3.2")
+//    implementation("org.apache.commons:commons-lang3:3.1")
+//    implementation("org.simplericity.macify:macify:1.6")
+//    implementation("javazoom:jlayer:1.0.1")
+//    implementation("com.googlecode.soundlibs:mp3spi:1.9.5.4")
+//    implementation("com.googlecode.soundlibs:tritonus-share:0.3.7.4")
 
 //    macBundleApp(":appbundler:1.0")
-
-//    implementation("org.jogl-all.jogl:jogl-all-natives-macosx-universal:v2.4.0-rc-20210111")
 
     implementation(files("lib/jogamp/gluegen-rt.jar"))
     implementation(files("lib/jogamp/jogl-all.jar"))
@@ -39,14 +45,12 @@ dependencies {
     implementation(files("lib/jogamp/gluegen-rt-natives-macosx-universal.jar"))
     implementation(files("lib/jogamp/jogl-all-natives-macosx-universal.jar"))
 
-
+//    implementation("org.jogl-all.jogl:jogl-all-natives-macosx-universal:v2.4.0-rc-20210111")
 //    macRuntime(":jogl-all-natives-macosx-universal:")
-    implementation("org.jogamp.jogl:jogl-all:2.3.2")
-    implementation("org.jogamp.gluegen:gluegen-rt:2.3.2")
-
+//    implementation("org.jogamp.jogl:jogl-all:2.3.2")
+//    implementation("org.jogamp.gluegen:gluegen-rt:2.3.2")
 //    implementation("org.jogamp.gluegen:gluegen-rt:")
 //    macRuntime(":gluegen-rt-natives-macosx-universal:")
-
 //    test("junit:junit:4.10")
 }
 
