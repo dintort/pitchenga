d19c4fe3c9ebcadc33657a4040ca22e4099fcd13
din tort <dintort@gmail.com>
2022-03-02 18:13:02 +0100

    Picked bass - reverting to fingered bass

--- src/main/java/com/pitchenga/Ptchng.java
+++ src/main/java/com/pitchenga/Ptchng.java
@@ -31,7 +31,7 @@ public class Ptchng extends Setup {
 //        riddleInstrument = Instrument.ELECTRIC_BASS_FINGER;
 //        riddleInstruments = new int[] { Instrument.ELECTRIC_PIANO_2 };
         riddleInstruments = new int[]{
-                Instrument.ELECTRIC_BASS_PICK,
+                Instrument.ELECTRIC_BASS_FINGER,
                 Instrument.ACOUSTIC_GRAND_PIANO,
                 Instrument.CHURCH_ORGAN,
                 Instrument.ELECTRIC_PIANO_1,
