5f949a03a418daa5f7b1b8e0e8c587e83e7a84d0
din tort <dintort@gmail.com>
2022-11-14 21:59:47 +0100

    Minor - location

--- src/main/java/com/harmoneye/app/AbstractHarmonEyeApp.java
+++ src/main/java/com/harmoneye/app/AbstractHarmonEyeApp.java
@@ -79,7 +79,8 @@ public class AbstractHarmonEyeApp {
         }
         frame.setLocationRelativeTo(null);
 //        frame.setLocation(0, 0);
-        frame.setLocation(0, 158);
+//        frame.setLocation(0, 158);
+        frame.setLocation(0, 1066);
 //        frame.setLocation(0, 220);
         frame.setJMenuBar(createMenuBar());
 
